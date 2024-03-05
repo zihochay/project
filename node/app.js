4205af33b2b68ceedac7692affddf14915c1321d
LAPTOP-249DPTM7\86158 <chenzhihao@youcash.com>
2024-03-05 18:51:31 +0800

    修改接口

--- node/app.js
+++ node/app.js
@@ -3,11 +3,17 @@ const Koa = require('koa');
 const path = require('path');
 
 const app = new Koa();
+const onerror = require('koa-onerror');
 const { koaBody } = require('koa-body');
 const json = require('koa-json');
 const logger = require('koa-logger');
 
+const responseFormatter = require('./middleware/response_formatter');
+const { apiPrefix } = require('./config/index');
 const routers = require('./routers/index');
+
+// koa的错误处理程序hack
+onerror(app);
 
 // middlewares
 app.use(koaBody({
@@ -27,7 +33,16 @@ app.use(koaBody({
 app.use(json());
 app.use(logger());
 
+// response formatter
+app.use(responseFormatter(apiPrefix));
+
 // routers
 app.use(routers.routes()).use(routers.allowedMethods());
+
+// 监听error
+app.on('error', (err, ctx) => {
+  // 在这里可以对错误信息进行一些处理，生成日志等。
+  console.error('server error', err, ctx);
+});
 
 app.listen(3000);
