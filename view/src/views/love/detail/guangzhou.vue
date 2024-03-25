2dccbc1d72a2c98b76a8b7713ebd697667659a19
LAPTOP-249DPTM7\86158 <772393327@qq.com>
2024-03-25 17:57:23 +0800

    增加原型飘动效果

--- view/src/views/love/detail/guangzhou.vue
+++ view/src/views/love/detail/guangzhou.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="guangzhou">
+    <div class="container">
+      <div class="cover"></div>
+    </div>
     <div class="box img img01 child-center">
       <div class="content01">
         <div class="addr-china">中国·广州</div>
@@ -115,5 +118,45 @@ export default {
 }
 .img04{
     background-image: url('@/assets/love-sh.png');
+}
+.container {
+  width: 200px;
+  height: 200px;
+  // background-color: #ccc;
+}
+.cover {
+  width: 99%;
+  height: 99%;
+  // background-color: red;
+  background-image: url("@/assets/love-01.jpg");
+  background-repeat: no-repeat;
+    background-position: center center; /* 将背景图居中显示 */
+    background-size: cover; /* 可选：根据容器大小调整背景图尺寸 */
+  -webkit-animation: transform 25s ease-in-out infinite both alternate;
+  animation: transform 25s ease-in-out infinite both alternate;
+  transform-style: preserve-3d;
+}
+@keyframes transform {
+0%, 100% {
+    border-radius: 63% 37% 54% 46%/55% 48% 52% 45%;
+}
+14% {
+    border-radius: 40% 60% 54% 46%/49% 60% 40% 51%;
+}
+28% {
+    border-radius: 54% 46% 38% 62%/49% 70% 30% 51%;
+}
+42% {
+    border-radius: 61% 39% 55% 45%/61% 38% 62% 39%;
+}
+56% {
+    border-radius: 61% 39% 67% 33%/70% 50% 50% 30%;
+}
+70% {
+    border-radius: 50% 50% 34% 66%/56% 68% 32% 44%;
+}
+84% {
+    border-radius: 46% 54% 50% 50%/35% 61% 39% 65%;
+}
 }
 </style>
