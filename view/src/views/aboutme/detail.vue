adafe621898806cfe97d04424ff367ee6281d264
LAPTOP-249DPTM7\86158 <772393327@qq.com>
2024-04-09 17:33:05 +0800

    增加详细页面问题导航和样式问题

--- view/src/views/aboutme/detail.vue
+++ view/src/views/aboutme/detail.vue
@@ -1,13 +1,58 @@
 <template>
   <div class="me-detail">
-    关于我的简历(详细版)
+    <div class="detail-menu hid-scroll">
+      <div class="menu-title" @click="scrollToSection('section1')">自我介绍</div>
+      <div class="menu-title">自我介绍自我介绍自我介绍自我介绍</div>
+      <div class="menu-title">自我介绍</div>
+      <div class="menu-title">自我介绍</div>
+    </div>
+    <div class="detail-main hid-scroll">
+      <myTitleVue title="自我介绍" id="section1"/>
+      <div class="br-left">
+        <div>1、熟练掌握JavaScript、HTML5 、CSS3和ES6的基本用法</div>
+        <div>2、掌握基本的网络知识，如http、tcp、cors、cookie和session等</div>
+        <div>3、掌握Vue、React、Echarts、JQuery、Jenkins 以及微信小程序 等一些技术和主流框架</div>
+        <div>4、熟练使用Vue 框架vue-router、Vue-cli 脚手架进行MVVM设计模式下的页面搭建与前后端数据交互</div>
+        <div>5、熟练使用 v-form 搭建动态表单项目，实现无代码拖拉拽生成表单页面</div>
+        <div>6、熟练使用ElementUI、Ant Designer、iview、vantUI等一些PC端和移动端的UI库</div>
+      </div>
+      <myTitleVue title="自我介绍"/>
+      <div class="br-left">
+        <div>1、熟练掌握JavaScript、HTML5 、CSS3和ES6的基本用法</div>
+        <div>2、掌握基本的网络知识，如http、tcp、cors、cookie和session等</div>
+        <div>3、掌握Vue、React、Echarts、JQuery、Jenkins 以及微信小程序 等一些技术和主流框架</div>
+        <div>4、熟练使用Vue 框架vue-router、Vue-cli 脚手架进行MVVM设计模式下的页面搭建与前后端数据交互</div>
+        <div>5、熟练使用 v-form 搭建动态表单项目，实现无代码拖拉拽生成表单页面</div>
+        <div>6、熟练使用ElementUI、Ant Designer、iview、vantUI等一些PC端和移动端的UI库</div>
+      </div>
+      <myTitleVue title="自我介绍"/>
+      <div class="br-left">
+        <div>1、熟练掌握JavaScript、HTML5 、CSS3和ES6的基本用法</div>
+        <div>2、掌握基本的网络知识，如http、tcp、cors、cookie和session等</div>
+        <div>3、掌握Vue、React、Echarts、JQuery、Jenkins 以及微信小程序 等一些技术和主流框架</div>
+        <div>4、熟练使用Vue 框架vue-router、Vue-cli 脚手架进行MVVM设计模式下的页面搭建与前后端数据交互</div>
+        <div>5、熟练使用 v-form 搭建动态表单项目，实现无代码拖拉拽生成表单页面</div>
+        <div>6、熟练使用ElementUI、Ant Designer、iview、vantUI等一些PC端和移动端的UI库</div>
+      </div>
+      <myTitleVue title="自我介绍"/>
+      <div class="br-left">
+        <div>1、熟练掌握JavaScript、HTML5 、CSS3和ES6的基本用法</div>
+        <div>2、掌握基本的网络知识，如http、tcp、cors、cookie和session等</div>
+        <div>3、掌握Vue、React、Echarts、JQuery、Jenkins 以及微信小程序 等一些技术和主流框架</div>
+        <div>4、熟练使用Vue 框架vue-router、Vue-cli 脚手架进行MVVM设计模式下的页面搭建与前后端数据交互</div>
+        <div>5、熟练使用 v-form 搭建动态表单项目，实现无代码拖拉拽生成表单页面</div>
+        <div>6、熟练使用ElementUI、Ant Designer、iview、vantUI等一些PC端和移动端的UI库</div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import myTitleVue from './components/my-title.vue'
+
 export default {
-  name: 'text-main',
+  name: 'me-detail',
   components: {
-  //   HelloWorld
+    myTitleVue
   },
   data () {
     return {
@@ -22,10 +67,67 @@ export default {
   mounted () {
   },
   methods: {
-
+    scrollToSection (sectionId) {
+      document.querySelector(`#${sectionId}`).scrollIntoView()
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.me-detail {
+  display: flex;
+  .detail-menu {
+    width: 20%;
+    box-sizing: border-box;
+    padding: 36px 10px 10px 20px;
+    height: calc(100vh - 62px);
+    border-right: 1px solid #ccc;
+    .menu-title {
+      // border: 1px solid pink;
+      line-height: 30px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      color: #488ff0;
+      &:hover {
+        cursor: pointer;
+        text-decoration: underline;
+      }
+    }
+  }
+  .detail-main {
+    width: 80%;
+    box-sizing: border-box;
+    padding: 0px 10px 10px 20px;
+    height: calc(100vh - 62px);
+  }
+  .br-left {
+    // height: 200px;
+    margin-left: 41px;
+    padding-top: 16px;
+    padding-left: 30px;
+    line-height: 28px;
+    border-left: 1px solid #c0ead8;
+  }
+// 隐藏滚动条
+.hid-scroll {
+  overflow: auto;
+  scrollbar-width: thin; /* 设置滚动条宽度 */
+  scrollbar-color: transparent transparent; /* 设置滚动条颜色为透明 */
+}
+/* 适用于 Safari 浏览器 */
+.hid-scroll::-webkit-scrollbar {
+  width: 6px; /* 设置滚动条宽度 */
+  height: 6px; /* 设置滚动条高度 */
+}
+
+.hid-scroll::-webkit-scrollbar-thumb {
+  background-color: transparent; /* 设置滚动条 thumb 的背景颜色为透明 */
+}
+
+.hid-scroll::-webkit-scrollbar-track {
+  background-color: transparent; /* 设置滚动条 track 的背景颜色为透明 */
+}
+}
 </style>
